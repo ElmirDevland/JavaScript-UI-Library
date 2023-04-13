43d58a30dbf7d3049d1a6ddcf23e110fa80ab56a
Elmir <elmir@whatever.com>
2023-04-13 21:10:27 +0400

    Modal and dropdown

---
+++ src/js/lib/components/modal.js
@@ -0,0 +1,29 @@
+import $ from '../core';
+
+$.prototype.modal = function () {
+  for (let i = 0; i < this.length; i++) {
+    const target = $(this[i]).getAttr('data-target');
+    $(this[i]).click((e) => {
+      e.preventDefault();
+      $(target).fadeIn(300);
+      document.body.style.overflow = 'hidden';
+    });
+  }
+
+  const closeElements = document.querySelectorAll('[data-close]');
+  closeElements.forEach((btn) => {
+    $(btn).click((e) => {
+      $('.modal').fadeOut(300);
+      document.body.style.overflow = '';
+    });
+  });
+
+  $('.modal').click((e) => {
+    if (e.target.classList.contains('modal')) {
+      $('.modal').fadeOut(300);
+      document.body.style.overflow = '';
+    }
+  });
+};
+
+$('[data-toggle="modal"]').modal();
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,1 @@
 import $ from './lib/lib';
-
-$('#first').click(() => {
-  $('.container').find('div').eq(0).fadeToggle(600);
-});
-
-$('[data-count="second"]').click(() => {
-  $('.container').find('div').eq(1).fadeToggle(600);
-});
-
-$('button')
-  .eq(2)
-  .click(() => {
-    $('.w-500px').fadeToggle(600);
-  });
